0518029a372c357c387c6fa8d92a63f5e74962ef
libradoj <librado.jimenez@tec.mx>
2021-08-08 13:20:58 -0600

    Ejercicios basicos 3 final

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,10 +1,6 @@
 def main():
     #escribe tu código abajo de esta línea
-    edad=int(input("Dame tu edad: "))
-    actual=int(input("Dame el año actual: "))
-    calculo=100-edad
-    tiempo=actual+calculo
-    print("Cumplirás 100 años en el año:",tiempo)
+    pass
 
 
 
